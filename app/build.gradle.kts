24fca8d1e507f64f9d149a1ac17fef06d07a70f3
RE <qq1609403959@outlook.com>
2025-06-19 23:23:55 +0800

    build(app): bump to 0.9.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 61
-        versionName = "0.9.6"
+        versionCode = 62
+        versionName = "0.9.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
