0f5103cc4e9a7c0d057b392dee5e81436e58a7ae
RE <qq1609403959@outlook.com>
2025-06-22 17:44:56 +0800

    chore: update versionCode to 65 and versionName to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "me.rerere.rikkahub"
     minSdk = 26
     targetSdk = 36
-    versionCode = 64
-    versionName = "1.0.0"
+    versionCode = 65
+    versionName = "1.0.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
