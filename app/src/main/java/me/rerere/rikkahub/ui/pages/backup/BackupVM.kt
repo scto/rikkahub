9bd039ed5afd9f0d90d9a2a3d25049f5528befd6
RE <qq1609403959@outlook.com>
2025-06-22 13:23:23 +0800

    feat: enhance restoreFromChatBox to support multiple providers

--- app/src/main/java/me/rerere/rikkahub/ui/pages/backup/BackupVM.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/backup/BackupVM.kt
@@ -1,15 +1,20 @@
 package me.rerere.rikkahub.ui.pages.backup
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.jsonArray
 import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import me.rerere.ai.provider.Modality
+import me.rerere.ai.provider.Model
+import me.rerere.ai.provider.ModelAbility
+import me.rerere.ai.provider.ProviderSetting
 import me.rerere.rikkahub.data.datastore.Settings
 import me.rerere.rikkahub.data.datastore.SettingsStore
 import me.rerere.rikkahub.data.sync.BackupFileItem
@@ -17,6 +22,8 @@ import me.rerere.rikkahub.data.sync.DataSync
 import me.rerere.rikkahub.utils.JsonInstant
 import me.rerere.rikkahub.utils.UiState
 import java.io.File
+
+private const val TAG = "BackupVM"
 
 class BackupVM(
   private val settingsStore: SettingsStore,
@@ -82,18 +89,78 @@ class BackupVM(
   }
 
   fun restoreFromChatBox(file: File) {
+    val importProviders = arrayListOf<ProviderSetting>()
+
     val jsonElements = JsonInstant.parseToJsonElement(file.readText()).jsonObject
-    val settings = jsonElements["settings"]?.jsonObject
-    if(settings != null) {
-      println(settings)
-      settings["providers"]?.jsonObject?.let { providers ->
+    val settingsObj = jsonElements["settings"]?.jsonObject
+    if (settingsObj != null) {
+      settingsObj["providers"]?.jsonObject?.let { providers ->
         providers["openai"]?.jsonObject?.let { openai ->
-
+          val apiHost = openai["apiHost"]?.jsonPrimitive?.contentOrNull ?: "https://api.openai.com"
+          val apiKey = openai["apiKey"]?.jsonPrimitive?.contentOrNull ?: ""
+          val models = openai["models"]?.jsonArray?.map { element ->
+            val modelId = element.jsonObject["modelId"]?.jsonPrimitive?.contentOrNull ?: ""
+            val capabilities =
+              element.jsonObject["capabilities"]?.jsonArray?.map { it.jsonPrimitive.contentOrNull }
+                ?: emptyList()
+            Model(
+              modelId = modelId,
+              displayName = modelId,
+              inputModalities = buildList {
+                if (capabilities.contains("vision")) {
+                  add(Modality.IMAGE)
+                }
+              },
+              abilities = buildList {
+                if (capabilities.contains("tool_use")) {
+                  add(ModelAbility.TOOL)
+                }
+                if (capabilities.contains("reasoning")) {
+                  add(ModelAbility.REASONING)
+                }
+              }
+            )
+          } ?: emptyList()
+          if(apiKey.isNotBlank()) importProviders.add(
+            ProviderSetting.OpenAI(
+              name = "OpenAI",
+              baseUrl = "$apiHost/v1",
+              apiKey = apiKey,
+              models = models,
+            )
+          )
         }
-        val claude = providers["claude"]?.jsonObject
-        val gemini = providers["gemini"]?.jsonObject
-
+        providers["claude"]?.jsonObject?.let { claude ->
+          val apiHost =
+            claude["apiHost"]?.jsonPrimitive?.contentOrNull ?: "https://api.anthropic.com"
+          val apiKey = claude["apiKey"]?.jsonPrimitive?.contentOrNull ?: ""
+          if(apiKey.isNotBlank()) importProviders.add(
+            ProviderSetting.Claude(
+              name = "Claude",
+              baseUrl = "${apiHost}/v1",
+              apiKey = apiKey,
+            )
+          )
+        }
+        providers["gemini"]?.jsonObject?.let { gemini ->
+          val apiHost = gemini["apiHost"]?.jsonPrimitive?.contentOrNull
+            ?: "https://generativelanguage.googleapis.com"
+          val apiKey = gemini["apiKey"]?.jsonPrimitive?.contentOrNull ?: ""
+          if(apiKey.isNotBlank()) importProviders.add(
+            ProviderSetting.Google(
+              name = "Gemini",
+              baseUrl = "$apiHost/v1beta",
+              apiKey = apiKey,
+            )
+          )
+        }
       }
     }
+
+    Log.i(TAG, "restoreFromChatBox: import ${importProviders.size} providers: $importProviders")
+
+    updateSettings(settings.value.copy(
+      providers = importProviders + settings.value.providers,
+    ))
   }
 }
