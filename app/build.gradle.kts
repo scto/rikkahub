7a7c5971ef75a6c4fbc8ac321863397fd334dd15
RE <qq1609403959@outlook.com>
2025-06-30 00:39:04 +0800

    build: 更新应用版本信息
    - 将 versionCode 从 67 修改为 68
    - 将 versionName 从 "1.0.3" 修改为 "1.1.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "me.rerere.rikkahub"
     minSdk = 26
     targetSdk = 36
-    versionCode = 67
-    versionName = "1.0.3"
+    versionCode = 68
+    versionName = "1.1.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
