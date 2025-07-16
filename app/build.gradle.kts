999f1434f24645d585d1202ce8e6a6cb259238b1
RE <qq1609403959@outlook.com>
2025-07-16 23:16:46 +0800

    build(app): 更新应用版本
    - 将版本代码从 73 修改为 74
    -将版本名称从 1.3.1 修改为 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "me.rerere.rikkahub"
     minSdk = 26
     targetSdk = 36
-    versionCode = 73
-    versionName = "1.3.1"
+    versionCode = 74
+    versionName = "1.3.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
