820a5d9191be887a1380302f149039c614986c0a
re-ovo <1609403959@qq.com>
2025-07-31 19:49:21 +0800

    feat(imggen): 实现 AI 生图页面布局

--- app/src/main/java/me/rerere/rikkahub/ui/pages/imggen/ImgGenPage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/imggen/ImgGenPage.kt
@@ -1,9 +1,89 @@
 package me.rerere.rikkahub.ui.pages.imggen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import com.composables.icons.lucide.GalleryVertical
+import com.composables.icons.lucide.Images
+import com.composables.icons.lucide.Lucide
+import com.composables.icons.lucide.Palette
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun ImageGenPage(modifier: Modifier = Modifier) {
-
+    val pagerState = rememberPagerState { 2 }
+    val scope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            Text("AI生图")
+        },
+        bottomBar = {
+            BottomBar(pagerState, scope)
+        }
+    ) { innerPadding ->
+        HorizontalPager(
+            state = pagerState,
+            modifier = modifier.padding(innerPadding)
+        ) { page ->
+            when (page) {
+                0 -> ImageGenScreen()
+                1 -> ImageGalleryScreen()
+            }
+        }
+    }
 }
+
+@Composable
+private fun BottomBar(
+    pagerState: PagerState,
+    scope: CoroutineScope
+) {
+    NavigationBar {
+        NavigationBarItem(
+            selected = 0 == pagerState.currentPage,
+            label = {
+                Text("图片生成")
+            },
+            icon = {
+                Icon(Lucide.Palette, null)
+            },
+            onClick = {
+                scope.launch {
+                    pagerState.animateScrollToPage(0)
+                }
+            }
+        )
+
+        NavigationBarItem(
+            selected = 1 == pagerState.currentPage,
+            label = {
+                Text("相册")
+            },
+            icon = {
+                Icon(Lucide.Images, null)
+            },
+            onClick = {
+                scope.launch {
+                    pagerState.animateScrollToPage(1)
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun ImageGenScreen() {}
+
+@Composable
+private fun ImageGalleryScreen() {}
