f844c8c20710a3c29bd38c70605751be7f3286aa
RE <qq1609403959@outlook.com>
2025-08-09 11:17:49 +0800

    chore: bump to 1.4.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 84
-        versionName = "1.4.10"
+        versionCode = 85
+        versionName = "1.4.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
